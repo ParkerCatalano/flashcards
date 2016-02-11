e1e8c9c2fb463f9df6533c4e7c921515abd5d9f5
Mulu Habtemariam and Parker Catalano <parkercatalano@gmail.com>
2016-02-10 21:12:43 -0800

    add a button that appends definition

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(function() {
     $(".initially-showing").fadeToggle();
     $(".initially-hidden").fadeToggle();
   });
+
+  $("button#functionDef").click(function() {
+    $("li").append("A JavaScript is a block of code designed to perform a particular task. A JavaScript function is executed when something invokes it calls it.");
+  });
+    $("button#functionDef").children("li").first().click(function() {
+      $(this).remove();
+  });
 });
